8d693b9f48efee463fafc95f562e95b037c8cef8
Jeremy Yallop <yallop@gmail.com>
2014-12-10 10:08:59 +0000

    Print out word size

--- dumpenv.ml
+++ dumpenv.ml
@@ -3,5 +3,6 @@ let (=>) k v = Printf.printf "%s: %s\n" k v
 let () = 
   begin
     "os_type" => Sys.os_type;
-    "ocaml_version" => Sys.ocaml_version
+    "ocaml_version" => Sys.ocaml_version;
+    "word_size" => (string_of_int Sys.word_size);
   end
